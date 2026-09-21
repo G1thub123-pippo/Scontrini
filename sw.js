871d39a865e23353e467d3135082ead1c70a3a65
G1thub123-pippo <albechims@gmail.com>
2026-09-21 23:38:01 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE='scontrini-v1.9.7';
+const FILES=['./index.html','./manifest.json','./sw.js','./icon.svg'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(
+    caches.keys().then(ks=>Promise.all(
+      ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  const isShell =
+    e.request.method==='GET' &&
+    (url.pathname.endsWith('/index.html') ||
+     url.pathname.endsWith('/manifest.json'));
+
+  if(isShell){
+    e.respondWith(
+      fetch(e.request, {cache:'no-store'})
+        .then(r=>{
+          const copy=r.clone();
+          caches.open(CACHE).then(c=>c.put(e.request,copy));
+          return r;
+        })
+        .catch(()=>caches.match(e.request))
+    );
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
+  );
+});
